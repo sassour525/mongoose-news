2df1887211be428f547c57b77d89a6236974f4e7
Shawn Assour <sassour@Shawns-MacBook-Pro.local>
2017-05-24 15:49:04 -0500

    added remote mongoLab db connection

--- server.js
+++ server.js
@@ -161,6 +161,6 @@ app.delete('/deleteNote/:id', function(req, res) {
 });
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("App running on port 3000!");
 });
