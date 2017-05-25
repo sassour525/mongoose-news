869452a97b68e0f8d7972811cee196a58dbcc127
Shawn Assour <sassour@Shawns-MacBook-Pro.local>
2017-05-25 13:35:47 -0500

    added more styling

--- public/js/saved.js
+++ public/js/saved.js
@@ -13,8 +13,9 @@ $(document).ready(function() {
         }).done(function(data) {
             if (data.notes && data.notes.length > 0) {
                 for (var i = 0; i < data.notes.length; i++) {
-                    $('.note-div').append('<h2>' + data.notes[i].body + '</h2>');
-                    $('.note-div').append('<button type="button" data-value="' + data.notes[i]._id + '" class="btn btn-danger delete-note-button" data-dismiss="modal">X</button>');
+                    $('.note-div').append('<h3>' + data.notes[i].body + '</h3>');
+                    $('.note-div').append('<button type="button" data-value="' + data.notes[i]._id + '" class="btn btn-danger delete-note-button pull-right btn-sm" data-dismiss="modal">X</button>');
+                    $('.note-div').append('<hr>');
                 }
             } else {
                 $('.note-div').html("No Notes");
